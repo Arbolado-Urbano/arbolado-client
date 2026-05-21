d614e41367c7d48f84993c31153eed6b0abee785
Ferares <fermin@ares.uy>
2026-05-21 16:41:23 -0300

    Add satellite map layer

--- src/elements/GeoInput/GeoInput.ts
+++ src/elements/GeoInput/GeoInput.ts
@@ -10,20 +10,29 @@ export default class GeoInput extends HTMLElement {
   private geoBtn: GeoBtn
   private map?: L.Map // Map reference
   private marker?: L.Marker // Marker
+  private baseLayers = {
+    street: L.tileLayer(
+      'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
+      {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        subdomains: 'abcd',
+        maxZoom: 21
+      },
+    ),
+    satellite: L.tileLayer(
+      `https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/{z}/{x}/{y}?access_token=${import.meta.env.VITE_MAPBOX_TOKEN}`,
+      {
+        maxZoom: 19,
+        tileSize: 256,
+        attribution: '&copy; <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      },
+    ),
+  }
   private mapOptions: L.MapOptions = { // Map options
     dragging: !L.Browser.mobile, // Disable one finger dragging on mobile devices
     center: L.latLng(-34.618, -58.44), // BsAs
-    layers: [
-      L.tileLayer(
-        'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
-        {
-          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-          subdomains: 'abcd',
-          maxZoom: 21
-        },
-      ),
-    ],
-    maxZoom: 21,
+    layers: [this.baseLayers.street],
+    maxZoom: 19,
     minZoom: 5,
     zoom: 14,
   }
@@ -50,6 +59,7 @@ export default class GeoInput extends HTMLElement {
       this.setValue(event.latlng)
     })
     this.map.on('move', () => this.map && this.addressLookup.setBounds(this.map.getBounds()))
+    L.control.layers(this.baseLayers, undefined, { position: "bottomleft" }).addTo(this.map)
     this.addressLookup.addEventListener('arbolado:address/selected', (event) => this.setValue((event as CustomEvent).detail.latLng))
   }
 
